04c7e3893c38a5bb9a09a696d5655a89e8c27bb6
Aaron Primmer <aaronmprimmer@hotmail.com>
2014-01-19 23:08:29 -0500

    Initialized Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webprimmer::Application.config.secret_token = 'b181706876ddea4cba30971c7e56bbf50ea825c008f9a4b402dc80723595d2ae52702503d290011372c7f35804410d256164254f3db7ce9e36bd72fcb0aaedec'
