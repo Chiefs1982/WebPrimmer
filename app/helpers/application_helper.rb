3ca92721ff090b14bb064172834f52bdfbfae62d
Aaron Primmer <aaronmprimmer@hotmail.com>
2014-01-20 13:59:03 -0500

    Added webprimmer layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  # Return a title on a per page basis
+  def title
+    base_title = "WebPrimmer.com"
+    
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
+  # Returns the image tag for the logo
+  def logo
+    image_tag("mainTitle.png", :alt => "WebPrimmer.com", :class => "logoPng")
+  end
+  
+  # Returns the image tag for upper left logo
+  def upper_logo
+    image_tag("appLogo.png", :alt => "WebPrimmer.com" )
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Webprimmer::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+  
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
